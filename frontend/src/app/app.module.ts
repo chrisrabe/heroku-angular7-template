2b1f513f4107a39c2df56891ad4f9853d4355cf0
chrisrabe <chris.rabe14@gmail.com>
2019-02-08 15:54:23 +1000

    restarted frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
