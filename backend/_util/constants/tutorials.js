c0089e3d54331195c0e98ce18049662a37eb6661
chrisrabe <chris.rabe14@gmail.com>
2019-02-16 12:01:57 +1000

    updated tutorials model

--- backend/_util/constants/tutorials.js
+++ backend/_util/constants/tutorials.js
@@ -3,17 +3,17 @@ const { TUTORIAL_LINKS } = require('./tutorial.links');
 
 const tutorials = [
     {
-        name: 'Angular',
+        title: 'Angular Resources',
         type: TUTORIALS.angular,
         links: TUTORIAL_LINKS.angular,
     },
     {
-        name: 'Heroku',
+        title: 'Heroku Resources',
         type: TUTORIALS.heroku,
         links: TUTORIAL_LINKS.heroku,
     },
     {
-        name: 'NodeJS',
+        title: 'NodeJS Resources',
         type: TUTORIALS.nodejs,
         links: TUTORIAL_LINKS.nodejs,
     }
